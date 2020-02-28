d81d64de8b8678868b0d157835f9804ea3756edf
Yiren Wang <yiren@illinois.edu>
2020-02-28 15:14:01 -0800

    multimt baseline

---
+++ Transformer/fairseq/tasks/translation_mtl.py
@@ -0,0 +1,291 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree. An additional grant of patent rights
+# can be found in the PATENTS file in the same directory.
+
+from collections import OrderedDict
+import copy
+import itertools
+import os
+
+import torch
+
+from fairseq import options, utils
+from fairseq.data import (
+    Dictionary,
+    ConcatDataset,
+    data_utils,
+    LanguagePairLangidDataset,
+    indexed_dataset,
+)
+from . import FairseqTask, register_task
+
+
+def _lang_token(lang: str):
+    return '__{}__'.format(lang)
+
+
+def _lang_token_index(dic: Dictionary, lang: str):
+    """Return language token index."""
+    idx = dic.index(_lang_token(lang))
+    assert idx != dic.unk_index, \
+        'cannot find language token for lang {}'.format(lang)
+    return idx
+
+
+def load_langpair_langid_dataset(
+    data_path, split, lang_pairs, src_dict, tgt_dict,
+    combine, dataset_impl, upsample_primary,
+    left_pad_source, left_pad_target, max_source_positions, max_target_positions,
+    encoder_langtok, decoder_langtok
+):
+    def split_exists(split, src, tgt, lang, data_path):
+        filename = os.path.join(data_path, '{}.{}-{}.{}'.format(split, src, tgt, lang))
+        return indexed_dataset.dataset_exists(filename, impl=dataset_impl)
+
+    src_datasets = []
+    tgt_datasets = []
+    src_langs, tgt_langs = [], []
+
+    for lang_pair in lang_pairs:
+        src, tgt = lang_pair.split("-")
+        for k in itertools.count():
+            split_k = split + (str(k) if k > 0 else '')
+
+            # infer langcode
+            if split_exists(split_k, src, tgt, src, data_path):
+                prefix = os.path.join(data_path, '{}.{}-{}.'.format(split_k, src, tgt))
+            elif split_exists(split_k, tgt, src, src, data_path):
+                prefix = os.path.join(data_path, '{}.{}-{}.'.format(split_k, tgt, src))
+            else:
+                if k > 0:
+                    break
+                else:
+                    raise FileNotFoundError('Dataset not found: {} ({})'.format(split, data_path))
+
+            src_datasets.append(indexed_dataset.make_dataset(prefix + src, impl=dataset_impl,
+                                                            fix_lua_indexing=True, dictionary=src_dict))
+            tgt_datasets.append(indexed_dataset.make_dataset(prefix + tgt, impl=dataset_impl,
+                                                            fix_lua_indexing=True, dictionary=tgt_dict))
+
+            print('| {} {} {}-{} {} examples'.format(data_path, split_k, src, tgt, len(src_datasets[-1])))
+            src_langs += [src] * len(src_datasets[-1])
+            tgt_langs += [tgt] * len(src_datasets[-1])
+
+            if not combine:
+                break
+
+    assert len(src_datasets) == len(tgt_datasets)
+    assert len(src_langs) == len(tgt_langs)
+
+    if len(src_datasets) == 1:
+        src_dataset, tgt_dataset = src_datasets[0], tgt_datasets[0]
+    else:
+        sample_ratios = [1] * len(src_datasets)
+        sample_ratios[0] = upsample_primary
+        src_dataset = ConcatDataset(src_datasets, sample_ratios)
+        tgt_dataset = ConcatDataset(tgt_datasets, sample_ratios)
+
+    return LanguagePairLangidDataset(
+        src_dataset, src_dataset.sizes, src_dict, src_langs,
+        tgt_dataset, tgt_dataset.sizes, tgt_dict, tgt_langs,
+        left_pad_source=left_pad_source,
+        left_pad_target=left_pad_target,
+        shuffle=True,
+        max_source_positions=max_source_positions,
+        max_target_positions=max_target_positions,
+        encoder_langtok=encoder_langtok,
+        decoder_langtok=decoder_langtok
+    )
+
+
+@register_task('translation_mtl')
+class TranslationMtlTask(FairseqTask):
+    """A task for training multiple translation models simultaneously.
+
+    Different from multilingual_translation task that iterate over different languages
+    Each batch consists of randomly sampled sentences from different language pairs
+    """
+
+    @staticmethod
+    def add_args(parser):
+        """Add task-specific arguments to the parser."""
+        # fmt: off
+        parser.add_argument('data', metavar='DIR', help='path to data directory')
+        parser.add_argument('--lang-pairs', default=None, metavar='PAIRS',
+                            help='comma-separated list of language pairs: en-de,en-fr,de-fr')
+        parser.add_argument('-s', '--source-lang', default=None, metavar='SRC',
+                            help='source language (only needed for inference)')
+        parser.add_argument('-t', '--target-lang', default=None, metavar='TARGET',
+                            help='target language (only needed for inference)')
+        parser.add_argument('--lazy-load', action='store_true',
+                            help='load the dataset lazily')
+        parser.add_argument('--raw-text', default=False, action='store_true',
+                            help='load raw text dataset')
+        parser.add_argument('--left-pad-source', default='True', type=str, metavar='BOOL',
+                            help='pad the source on the left (default: True)')
+        parser.add_argument('--left-pad-target', default='False', type=str, metavar='BOOL',
+                            help='pad the target on the left (default: False)')
+        parser.add_argument('--max-source-positions', default=1024, type=int, metavar='N',
+                            help='max number of tokens in the source sequence')
+        parser.add_argument('--max-target-positions', default=1024, type=int, metavar='N',
+                            help='max number of tokens in the target sequence')
+        parser.add_argument('--upsample-primary', default=1, type=int,
+                            help='amount to upsample primary dataset')
+        parser.add_argument('--encoder-langtok', default='tgt', type=str, choices=['src', 'tgt'],
+                            metavar='SRCTGT',
+                            help='replace beginning-of-sentence in source sentence with source or target '
+                                 'language token. (src/tgt) default to be target lan_id')
+        parser.add_argument('--decoder-langtok', action='store_true',
+                            help='replace beginning-of-sentence in target sentence with target language token')
+    
+    def __init__(self, args, src_dict, tgt_dict, training):
+        super().__init__(args)
+        self.src_dict = src_dict
+        self.tgt_dict = tgt_dict
+        self.lang_pairs = args.lang_pairs
+        # eval_lang_pairs for multilingual translation is usually all of the
+        # lang_pairs. However for other multitask settings or when we want to
+        # optimize for certain languages we want to use a different subset. Thus
+        # the eval_lang_pairs class variable is provided for classes that extend
+        # this class.
+        self.eval_lang_pairs = args.lang_pairs
+        # model_lang_pairs will be used to build encoder-decoder model pairs in
+        # models.build_model(). This allows multitask type of sub-class can
+        # build models other than the input lang_pairs
+        self.model_lang_pairs = copy.copy(args.lang_pairs)
+        self.langs = sorted(list({x for lang_pair in args.lang_pairs for x in lang_pair.split('-')}))
+        self.training = training
+
+    @classmethod
+    def setup_task(cls, args, **kwargs):
+        src_dict, tgt_dict, training = cls.prepare(args, **kwargs)
+        return cls(args, src_dict, tgt_dict, training)
+    
+    @classmethod
+    def prepare(cls, args, **kargs):
+        args.left_pad_source = options.eval_bool(args.left_pad_source)
+        args.left_pad_target = options.eval_bool(args.left_pad_target)
+        if getattr(args, 'raw_text', False):
+            utils.deprecation_warning('--raw-text is deprecated, please use --dataset-impl=raw')
+            args.dataset_impl = 'raw'
+        elif getattr(args, 'lazy_load', False):
+            utils.deprecation_warning('--lazy-load is deprecated, please use --dataset-impl=lazy')
+            args.dataset_impl = 'lazy'
+
+        if args.lang_pairs is None:
+            raise ValueError('--lang-pairs is required. List all the language pairs in the training objective.')
+        args.lang_pairs = args.lang_pairs.split(',')
+        sorted_langs = sorted(list({x for lang_pair in args.lang_pairs for x in lang_pair.split('-')}))
+        if args.source_lang is not None or args.target_lang is not None:
+            training = False
+            args.lang_pairs = ['{}-{}'.format(args.source_lang, args.target_lang)]
+        else:
+            training = True
+            args.source_lang, args.target_lang = "src", "tgt"
+
+        # load dictionaries
+        paths = args.data.split(':')
+        assert len(paths) > 0
+        src_dict = cls.load_dictionary(os.path.join(paths[0], 'dict.src.txt'))
+        tgt_dict = cls.load_dictionary(os.path.join(paths[0], 'dict.tgt.txt'))
+        assert src_dict.pad() == tgt_dict.pad()
+        assert src_dict.eos() == tgt_dict.eos()
+        assert src_dict.unk() == tgt_dict.unk()
+        # add language token
+        if args.encoder_langtok is not None or args.decoder_langtok:
+            for lang_to_add in sorted_langs:
+                src_dict.add_symbol(_lang_token(lang_to_add))
+                tgt_dict.add_symbol(_lang_token(lang_to_add))
+        print('| [src] dictionary: {} types'.format(len(src_dict)))
+        print('| [tgt] dictionary: {} types'.format(len(tgt_dict)))
+        return src_dict, tgt_dict, training
+    
+    
+    def load_dataset(self, split, epoch=0, combine=False, **kwargs):
+        """Load a given dataset split.
+
+        Args:
+            split (str): name of the split (e.g., train, valid, test)
+        """
+        paths = self.args.data.split(':')
+        assert len(paths) > 0
+        data_path = paths[epoch % len(paths)]
+
+        self.datasets[split] = load_langpair_langid_dataset(
+            data_path, split, self.lang_pairs, self.src_dict, self.tgt_dict,
+            combine=combine, dataset_impl=self.args.dataset_impl,
+            upsample_primary=self.args.upsample_primary,
+            left_pad_source=self.args.left_pad_source,
+            left_pad_target=self.args.left_pad_target,
+            max_source_positions=self.args.max_source_positions,
+            max_target_positions=self.args.max_target_positions,
+            encoder_langtok=self.args.encoder_langtok,
+            decoder_langtok=self.args.decoder_langtok
+        )
+    
+    def build_dataset_for_inference(self, src_tokens, src_lengths):
+        src_langs = [self.args.source_lang] * len(src_lengths)
+        tgt_langs = [self.args.target_lang] * len(src_lengths)
+        return LanguagePairLangidDataset(
+            src_tokens, src_lengths, self.source_dictionary,
+            src_langs, tgt_langs=tgt_langs
+        )
+
+    def train_step(self, sample, model, criterion, optimizer, ignore_grad=False):
+        """
+        Do forward and backward, and return the loss as computed by *criterion*
+        for the given *model* and *sample*.
+
+        Args:
+            sample (dict): the mini-batch. The format is defined by the
+                :class:`~fairseq.data.FairseqDataset`.
+            model (~fairseq.models.BaseFairseqModel): the model
+            criterion (~fairseq.criterions.FairseqCriterion): the criterion
+            optimizer (~fairseq.optim.FairseqOptimizer): the optimizer
+            ignore_grad (bool): multiply loss by 0 if this is set to True
+
+        Returns:
+            tuple:
+                - the loss
+                - the sample size, which is used as the denominator for the
+                  gradient
+                - logging outputs to display while training
+        """
+        model.train()
+
+        """
+        print("[debug]==========================")
+        print("sample:    {}".format(type(sample)))
+        # print(sample)
+        print("\nsrc_tokens:")
+        for s in sample['net_input']['src_tokens']:
+            # print(s)
+            print(self.src_dict.string(s))
+        print("\ntarget:")
+        for s in sample['target']:
+            print(self.src_dict.string(s))
+        print("[debug]==========================")
+        """
+        loss, sample_size, logging_output = criterion(model, sample)
+        if ignore_grad:
+            loss *= 0
+        optimizer.backward(loss)
+        return loss, sample_size, logging_output
+
+    def max_positions(self):
+        """Return the max sentence length allowed by the task."""
+        return (self.args.max_source_positions, self.args.max_target_positions)
+
+    @property
+    def source_dictionary(self):
+        """Return the source :class:`~fairseq.data.Dictionary`."""
+        return self.src_dict
+
+    @property
+    def target_dictionary(self):
+        """Return the target :class:`~fairseq.data.Dictionary`."""
+        return self.tgt_dict
+
